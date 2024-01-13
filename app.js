0767b2b970675a97f80e7d3d17586a96eed31be8
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2024-01-13 20:07:06 +0100

    front end of the app and added cloudinary keys

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const express = require("express");
+require('dotenv').config();
+
+
+const express = require('express');
 const app = express();
+
+const fileUpload = require('express-fileupload');
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+const DataBase = require('./database');
+
+
+
+app.use(express.static('./public'));
+
+app.use(express.json());
+app.use(fileUpload({ useTempFiles: true }));
+
+app.get('/', (req, res) => {
+  res.send('<h1>File Upload Starter</h1>');
+});
+
+
 
 const port = process.env.PORT || 5000;
 
 const start = async () => {
-  try {    
+  try {
+    await DataBase(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const DataBase=(url)=>{
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+      })
+}
+ module.exports = DataBase;
